5c6c8799bd30f5bcd6f586fc5bd8add02c8fc186
ravi-gaurav <ravigaurav2509@gmail.com>
2021-07-21 20:00:27 +0530

    Removed rows with null values in below mentioned columns->
    1. DATE_OF_REGISTRATION
    2. COMPANY_CLASS
    3. REGISTERED_OFFICE_ADDRESS

--- DataPreprocessing/dataExploration1.py
+++ DataPreprocessing/dataExploration1.py
@@ -8,20 +8,38 @@ spark = SparkSession \
         .getOrCreate()
 
 
-winutilsPath = 'E:/studies/winutils-master/hadoop-2.7.1'
-datasetPath = 'C:/Users/SkJain/Documents/BigDataWorkspace2/registered_companies.csv'
+# winutilsPath = 'E:/studies/winutils-master/hadoop-2.7.1'
 
-#read the dataset
+# datasetPath = 'C:/Users/SkJain/Documents/BigDataWorkspace2/registered_companies.csv'
+datasetPath = '/Users/ravigaurav/My Data/Personal Projects/Datasets/registered_companies.csv'
+
+# read the dataset
 companies_data_raw = spark.read.option("header", True).csv(datasetPath)
 # companies_data_raw.show(truncate=False)
 
-#list of columns in the dataset
+# list of columns in the dataset
 colms_list = companies_data_raw.columns
 for coln in colms_list:
         print(coln)
         
-#list of columns with the datatypes
+# list of columns with the datatypes
 for col_types in companies_data_raw.dtypes:
         print(col_types)
+
+
+# Removing all the companies where date of registration is null
+# Total number of companies = 1992170
+# Number of companies with non-null DOR = 1989645
+comp_data1 = companies_data_raw.filter('DATE_OF_REGISTRATION is not NULL')
+
+# Removing all the companies where company class is null
+# Number of companies with non-null COMPANY_CLASS = 1985416
+comp_data2 = comp_data1.filter('COMPANY_CLASS is not NULL')
+
+# Removing all the companies with null REGISTERED_OFFICE_ADDRESS
+# Number of companies with non-null REGISTERED_OFFICE_ADDRESS = 1971833
+comp_data3 = comp_data2.filter('REGISTERED_OFFICE_ADDRESS is not NULL')
+
+
 
 spark.stop()
